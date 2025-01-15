782d470c30786966a51a0f23b31ec287be43bebf
YangSeungMin <ysmworkplace@gmail.com>
2025-01-15 18:01:09 +0900

    젠킨스 테스트를 위한 임시 데이터소스 주석

--- src/main/java/com/min/mms/MmsApplication.java
+++ src/main/java/com/min/mms/MmsApplication.java
@@ -2,8 +2,9 @@ package com.min.mms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MmsApplication {
 
     public static void main(String[] args) {
