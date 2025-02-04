0271c664f9b49baca8f91c6d3a7f3f7167ec938b
YangSeungMin <ysmworkplace@gmail.com>
2025-02-04 15:28:08 +0900

    관측소 정보 관련 구조생성

--- src/main/java/com/min/mms/menu/station/controller/StationController.java
+++ src/main/java/com/min/mms/menu/station/controller/StationController.java
@@ -1,14 +1,28 @@
 package com.min.mms.menu.station.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/station")
 public class StationController {
 
-    @GetMapping("/station/read")
-    public String stationRead() {
-        return "menu/station/station_read";
+    @GetMapping("/stations")
+    public String stations(Model model) {
+        return "menu/station/station_select";
+    }
+
+    @GetMapping("/{station_name}")
+    public String stationDetail(@PathVariable String station_name, Model model) {
+        return "menu/station/station_detail";
+    }
+
+    @GetMapping("/create")
+    public String stationCreate() {
+        return "menu/station/station_create";
     }
 
 }
