0766a6a20c44a006a0dd4794286f6db423e1acfb
YangSeungMin <ysmworkplace@gmail.com>
2025-02-13 18:28:40 +0900

    GIS MAP 작업중

---
+++ src/main/resources/static/js/gismap/gismap.js
@@ -0,0 +1,18 @@
+$(function() {
+    backgroundMap();
+});
+
+function backgroundMap() {
+    let map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([127.024612, 37.532600]),
+            zoom: 10
+        })
+    });
+}
