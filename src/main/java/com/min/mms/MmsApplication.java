3ac1d11a64865e7d05b248fb6a489c78773f3530
YangSeungMin <ysmworkplace@gmail.com>
2025-02-25 15:10:04 +0900

    smtp 계정 디버깅

--- src/main/java/com/min/mms/MmsApplication.java
+++ src/main/java/com/min/mms/MmsApplication.java
@@ -1,13 +1,21 @@
 package com.min.mms;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MmsApplication {
 
+    @Value("${gmail}")
+    private static String mailUsername;
+
+    @Value("${gmailpw}")
+    private static String mailPassword;
+
     public static void main(String[] args) {
         SpringApplication.run(MmsApplication.class, args);
+        System.out.println(mailUsername + mailPassword);
     }
 
 }
